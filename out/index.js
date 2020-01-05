ee2d6704b157cc1dd8b9fdb2454d274d07b221c1
daniel <opodaniel@yahoo.com>
2020-01-05 22:22:02 +0200

    add last 2 solutions - wip sort score object for ex 5

---
+++ out/index.js
@@ -0,0 +1,45 @@
+"use strict";
+const data = [
+    {
+        "name": "John",
+        "type": "dog"
+    },
+    {
+        "name": "Sparky",
+        "type": "dog"
+    },
+    {
+        "name": "Nova",
+        "type": "cat"
+    },
+    {
+        "name": "Manuel",
+        "type": "dog"
+    },
+    {
+        "name": "Caesar",
+        "type": "cat"
+    },
+    {
+        "name": "Gill",
+        "type": "fish"
+    }
+];
+const groupByType = (ar) => {
+    return ar.reduce((acc, element) => {
+        const position = acc.findIndex((entry) => entry.type === element.type);
+        if (position === -1) {
+            acc.push({
+                type: element.type,
+                animals: [element.name]
+            });
+        }
+        else {
+            acc[position].animals.push(element.name);
+        }
+        ;
+        return acc;
+    }, []);
+};
+const elements = groupByType(data);
+console.log({ elements });
